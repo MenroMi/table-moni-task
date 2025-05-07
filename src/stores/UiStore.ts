3e6a2cc8b401dcb2b95d0a4a85aedba4ac446382
Kiryl Shchasny <folkbread@gmail.com>
2025-05-07 22:03:56 +0200

    fix: removed duplicated store property

--- src/stores/UiStore.ts
+++ src/stores/UiStore.ts
@@ -3,7 +3,6 @@ import { makeAutoObservable } from 'mobx';
 class UiStore {
   tableScrollLeft = 0;
   tableScrollAtEnd = false;
-  tableScrollRightEdge = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -11,7 +10,6 @@ class UiStore {
 
   setScroll(left: number, maxScroll: number, clientWidth: number) {
     this.tableScrollLeft = left;
-    this.tableScrollRightEdge = left + clientWidth >= maxScroll - 1;
     this.tableScrollAtEnd = left + clientWidth >= maxScroll - 1;
   }
 }
